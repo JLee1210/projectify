d178f11e1039e38d4a0adb1d20b10ef4bb3358da
Jaden Lee <jadenjlee@outlook.com>
2021-12-09 03:40:48 -0500

    Fix console warnings

--- src/components/EditDeleteButton.js
+++ src/components/EditDeleteButton.js
@@ -2,11 +2,11 @@ import { MDBIcon, MDBBtn } from 'mdbreact'
 
 export const EditDeleteButton = ({ onEdit, onDelete }) => (
     <div>
-        <MDBBtn floating size="sm" color="blue-grey" onClick={onEdit}>
+        <MDBBtn floating="true" size="sm" color="blue-grey" onClick={onEdit}>
             <MDBIcon icon="edit" />
         </MDBBtn>
 
-        <MDBBtn floating size="sm" color="blue-grey" onClick={onDelete}>
+        <MDBBtn floating="true" size="sm" color="blue-grey" onClick={onDelete}>
             <MDBIcon icon="trash" />
         </MDBBtn>
     </div>
