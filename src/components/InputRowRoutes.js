6224c4f1f687da187746b3c8fc8d139404c258ce
Jaden Lee <jadenjlee@outlook.com>
2021-12-09 02:13:21 -0500

    Add MajorRelationRow component

--- src/components/InputRowRoutes.js
+++ src/components/InputRowRoutes.js
@@ -1,6 +1,7 @@
-import { ClassRow } from './Rows/ClassRow'
+import { CourseRow } from './Rows/CourseRow'
 import { DepartmentRow } from './Rows/DepartmentRow'
 import { MajorRow } from './Rows/MajorRow'
+import { MajorRelationRow } from './Rows/MajorRelationRow'
 import { ProjectRow } from './Rows/ProjectRow'
 import { StudentRow } from './Rows/StudentRow'
 import { Route, Switch } from 'react-router-dom'
@@ -9,11 +10,16 @@ import { HomePage } from '../pages/HomePage'
 export const InputRowRoutes = () => (
     <Switch>
         <Route exact path="/projectify" component={HomePage} />
-        <Route exact path="/projectify/student" component={StudentRow} />
-        <Route exact path="/projectify/class" component={ClassRow} />
-        <Route exact path="/projectify/major" component={MajorRow} />
-        <Route exact path="/projectify/project" component={ProjectRow} />
+        <Route exact path="/projectify/class" component={CourseRow} />
         <Route exact path="/projectify/department" component={DepartmentRow} />
+        <Route exact path="/projectify/major" component={MajorRow} />
+        <Route
+            exact
+            path="/projectify/major_relation"
+            component={MajorRelationRow}
+        />
+        <Route exact path="/projectify/project" component={ProjectRow} />
+        <Route exact path="/projectify/student" component={StudentRow} />
         <Route
             render={() => (
                 <h1 className="blue-grey-text text-center">
---
+++ src/components/Rows/MajorRelationRow.js
@@ -0,0 +1,95 @@
+import {
+    Button,
+    Form,
+    FormGroup,
+    FormText,
+    Input,
+    Label,
+    Col,
+    Row,
+} from 'reactstrap'
+import { useContext, useState, useEffect } from 'react'
+import { TableContext } from '../../context/TableProvider'
+import axios from 'axios'
+import { majorRelationUrl } from '../../constants/endpoints'
+
+export const MajorRelationRow = () => {
+    const [formData, setFormData] = useState({
+        studentId: undefined,
+        majorId: undefined,
+    })
+    const { data, setTableType } = useContext(TableContext)
+
+    useEffect(() => {
+        setTableType('majorRelation')
+    }, [setTableType])
+
+    const onClickMajor = async (e) => {
+        e.preventDefault()
+        const response = await axios.post(majorRelationUrl, formData)
+        data.majorRelation.setTable(response.data.data)
+    }
+
+    return (
+        <div>
+            <h1 className="tc mb4">Major Relation Table</h1>
+            <div
+                id="add-row"
+                className="d-flex align-items-center justify-content-center"
+            >
+                <Form>
+                    <Row form>
+                        <Col sm={20}>
+                            <FormGroup>
+                                <Label for="studentId" sm={10}>
+                                    Student ID
+                                </Label>
+                                <Input
+                                    id="studentId"
+                                    name="studentId"
+                                    type="number"
+                                    size={10}
+                                    onChange={(e) =>
+                                        setFormData({
+                                            ...formData,
+                                            [e.target.name]: e.target.value,
+                                        })
+                                    }
+                                />
+                                <FormText>required</FormText>
+                            </FormGroup>
+                        </Col>
+                        <Col sm={20}>
+                            <FormGroup>
+                                <Label for="majorId" sm={10}>
+                                    Major ID
+                                </Label>
+                                <Input
+                                    id="majorId"
+                                    name="majorId"
+                                    type="number"
+                                    size={10}
+                                    onChange={(e) =>
+                                        setFormData({
+                                            ...formData,
+                                            [e.target.name]: e.target.value,
+                                        })
+                                    }
+                                />
+                                <FormText>required</FormText>
+                            </FormGroup>
+                        </Col>
+                        <Button
+                            color="blue-grey"
+                            className="h-25 m-auto"
+                            disabled={!formData.majorID || !formData.studentId}
+                            onClick={onClickMajor}
+                        >
+                            Add
+                        </Button>
+                    </Row>
+                </Form>
+            </div>
+        </div>
+    )
+}
